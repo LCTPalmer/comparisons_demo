5d39515e3d9f0fceb5ddbd884d2669fa50b9fa71
Luke <lctpalmer@gmail.com>
2016-06-13 17:42:05 +0100

    updated email script to output timestamp

--- app/database/email_db.sh
+++ app/database/email_db.sh
@@ -5,6 +5,7 @@ sqlite3 pairwise_comparisons.db ".backup pairwise_comparisons.db.bak"
 #compress the backup
 timestamp=$(date +%s)
 tar -cvzf "db_backups/db_backup${timestamp}.tar.gz" pairwise_comparisons.db.bak
+echo "${timestamp} db backup sent"
 
 #email
 mutt -s 'db backup' comparisons.demo@gmail.com -a "db_backups/db_backup${timestamp}.tar.gz"  < /dev/null
